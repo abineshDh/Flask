7d64b11780b6d07fd84d315d231ad70204060c5b
abineshDh <abineshspartan2001@gmail.com>
2025-05-25 12:20:37 +0530

    Authentication app using Flask-login and SQLAlchemy

--- Authentication-Flask-login/main.py
+++ Authentication-Flask-login/main.py
@@ -13,7 +13,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'HG2tJ89751CAA6251ATcgUp'
 
 # initialize database and login manager 
-db = SQLAlchemy(app) # efine models (database tables) and interact with the database using Python.
+db = SQLAlchemy(app) # define models (database tables) and interact with the database using Python.
 login_manager = LoginManager() # manages user sessions - login, logout, restrict acess
 login_manager.init_app(app) # manage user sessions - flasks request/response cycle
 login_manager.login_view = 'login' # specifies users to redirected to login page - with login route using login_view function
@@ -89,18 +89,30 @@ def login():
 
 # Protected dashboard route
 @app.route("/dashboard")
+# @login_required: You must be logged in or get redirected to login_view
 @login_required
 def dashboard():
+    # current_user: Flask-Login provides this object globally
     return render_template("dashboard.html", username=current_user.username)
 
 # Logout Route to end the session and redirect to home page/register
 @app.route('/logout', methods=['POST'])
 @login_required
 def logout():
+    # logout_user() clears the session. The user is no longer considered logged in.
     logout_user()
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+# | Feature            | Function                                  |
+# | ------------------ | ----------------------------------------- |
+# | `LoginManager`     | Manages login state, session, redirection |
+# | `UserMixin`        | Gives your model required properties      |
+# | `login_user(user)` | Logs in the user, stores session          |
+# | `logout_user()`    | Logs out the user, clears session         |
+# | `@login_required`  | Restricts access to logged-in users       |
+# | `current_user`     | Gives access to the current user object   |
 
 
